2c521620a79d837d121b3e50b566abb421a8b15d
betulcelebi <betulcelebi.05@gmail.com>
2022-10-23 14:07:40 +0300

    Spotify UI created

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Spotify UI',
+        title: 'Spotify UI App',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.blue,
